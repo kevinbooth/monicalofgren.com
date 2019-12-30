e8f4a138a98edf420fe2d87b6aae8f9fcbc21764
Kevin Booth <kdbooth97@gmail.com>
2019-12-30 15:44:30 -0500

    Add unknown page handling

--- server/app.js
+++ server/app.js
@@ -14,6 +14,10 @@ app.get('/api', function(req,res) {
   res.send('API Dir!')
 })
 
+app.get('*', function(req,res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '../client/dist/') })
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
